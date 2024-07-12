468972368531251d3858b3d5cfea995f6c09fc1c
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-07-12 20:00:33 +0700

    feat: added data from API

--- src/components/dashboard/MainDashboard.jsx
+++ src/components/dashboard/MainDashboard.jsx
@@ -1,3 +1,4 @@
+import {useEffect, useState} from 'react'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import DashboardHeader from "./DashboardHeader";
@@ -12,123 +13,156 @@ import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
 
 export default function MainDashboard() {
-  return (
-    <section className="h-screen font-poppins">
-      <DashboardHeader />
-      <Tabs defaultValue="account" className="w-full px-16 py-24">
-        <TabsList className="w-full mb-16 mt-6 flex gap-x-5">
-          <TabsTrigger className="w-full border border-slate-400 py-6" value="account">
-            <div className="flex items-center gap-x-5">
-              <div className={`bg-slate-300 h-14 w-14 flex items-center ${true ? "rounded-lg" : "rounded"} justify-center rounded-xl text-2xl`}>
-                <FaUsers />
-              </div>
-              <div className="flex flex-col justify-start">
-                <h1 className="text-xl font-semibold">Total anak</h1>
-                <p className="text-start text-lg">100</p>
-              </div>
+  const [parentTableData, setParentTableData] = useState([])
+  const [childTableData, setChildTableData] = useState([])
+
+  const getParentTableData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/parents')
+
+      if (response.data) {
+        console.log(response.data.data)
+        setParentTableData(response.data.data)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const getChildTableData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/childrens')
+
+      if (response.data) {
+        console.log(response.data.data)
+        setChildTableData(response.data.data)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getParentTableData()
+    getChildTableData()
+  }, []);
+
+  return <section className="h-screen font-poppins">
+    <DashboardHeader />
+    <Tabs defaultValue="parents" className="w-full px-16 py-24">
+      <TabsList className="w-full mb-16 mt-6 flex gap-x-5">
+        <TabsTrigger className="w-full border border-slate-400 py-6" value="childrens">
+          <div className="flex items-center gap-x-5">
+            <div className={`bg-slate-300 h-14 w-14 flex items-center ${true ? "rounded-lg" : "rounded"} justify-center rounded-xl text-2xl`}>
+              <FaUsers />
             </div>
-          </TabsTrigger>
-          <TabsTrigger className="w-full border border-slate-400 py-6" value="password">
-            <div className="flex items-center gap-x-5">
-              <div className={`bg-slate-300 h-14 w-14 flex items-center ${true ? "rounded-lg" : "rounded"} justify-center rounded-xl text-2xl`}>
-                <FaUser />
-              </div>
-              <div className="flex flex-col justify-start">
-                <h1 className="text-xl font-semibold">Total User</h1>
-                <p className="text-start text-lg">86</p>
-              </div>
-            </div>
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <div className="flex mb-5">
-            <div className="w-full">
-              <h1 className="font-semibold text-slate-400">Data Anak</h1>
-            </div>
-            <div className="w-full relative">
-              <CiSearch className="absolute text-xl right-3 top-2 text-black" />
-              <Input className="rounded-full border-slate-400" placeholder="Search" />
-            </div>
-            <div className="w-full text-end">
-              <Button>
-                <FaPlus />
-              </Button>
+            <div className="flex flex-col justify-start">
+              <h1 className="text-xl font-semibold">Anak-anak</h1>
+              <p className="text-start text-lg">100</p>
             </div>
           </div>
-          <Table>
-            <TableHeader>
+        </TabsTrigger>
+        <TabsTrigger className="w-full border border-slate-400 py-6" value="parents">
+          <div className="flex items-center gap-x-5">
+            <div className={`bg-slate-300 h-14 w-14 flex items-center ${true ? "rounded-lg" : "rounded"} justify-center rounded-xl text-2xl`}>
+              <FaUser />
+            </div>
+            <div className="flex flex-col justify-start">
+              <h1 className="text-xl font-semibold">Orang Tua</h1>
+              <p className="text-start text-lg">86</p>
+            </div>
+          </div>
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="childrens">
+        <div className="flex mb-5">
+          <div className="w-full">
+            <h1 className="font-semibold text-slate-400">Data Anak</h1>
+          </div>
+          <div className="w-full relative">
+            <CiSearch className="absolute text-xl right-3 top-2 text-black" />
+            <Input className="rounded-full border-slate-400" placeholder="Search" />
+          </div>
+          <div className="w-full text-end">
+            <Button>
+              <FaPlus />
+            </Button>
+          </div>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Nama Anak</TableHead>
+              <TableHead>Nama Orang Tua</TableHead>
+              <TableHead>Umur Anak</TableHead>
+              <TableHead>Jenis Kelamin (p / l) </TableHead>
+              <TableHead>Tinggi Badan (cm)</TableHead>
+              <TableHead>Berat Badan (kg)</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Edit Data</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {childTableData && childTableData.map((child) => (
               <TableRow>
-                <TableHead>Nama Anak</TableHead>
-                <TableHead>Nama Orang Tua</TableHead>
-                <TableHead>Umur Anak</TableHead>
-                <TableHead>Jenis Kelamin (p / l) </TableHead>
-                <TableHead>Tinggi Badan (cm)</TableHead>
-                <TableHead>Berat Badan (kg)</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Edit Data</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>Asep</TableCell>
-                <TableCell>Ujang</TableCell>
-                <TableCell>1 thn 2 bln</TableCell>
-                <TableCell>l</TableCell>
-                <TableCell>xx</TableCell>
-                <TableCell>xx</TableCell>
+                <TableCell>{child.name}</TableCell>
+                <TableCell>{child.name}</TableCell>
+                <TableCell>{child.age} Tahun</TableCell>
+                <TableCell>{child.gender === 1 ? 'Laki-laki' : 'Perempuan'}</TableCell>
+                <TableCell>{child.weight}</TableCell>
+                <TableCell>{child.height}</TableCell>
                 <TableCell>Normal</TableCell>
                 <TableActionButton />
               </TableRow>
-            </TableBody>
-          </Table>
-        </TabsContent>
-        <TabsContent value="password">
-          <div className="flex mb-5">
-            <div className="w-full">
-              <h1 className="font-semibold text-slate-400">Data Orang Tua</h1>
-            </div>
-            <div className="w-full relative">
-              <CiSearch className="absolute text-xl right-3 top-2 text-black" />
-              <Input className="rounded-full border-slate-400" placeholder="Search" />
-            </div>
-            <div className="w-full text-end">
-              <Button>
-                <FaPlus />
-              </Button>
-            </div>
+            ))}
+          </TableBody>
+        </Table>
+      </TabsContent>
+      <TabsContent value="parents">
+        <div className="flex mb-5">
+          <div className="w-full">
+            <h1 className="font-semibold text-slate-400">Data Orang Tua</h1>
           </div>
-          <Table>
-            <TableHeader>
+          <div className="w-full relative">
+            <CiSearch className="absolute text-xl right-3 top-2 text-black" />
+            <Input className="rounded-full border-slate-400" placeholder="Search" />
+          </div>
+          <div className="w-full text-end">
+            <Button>
+              <FaPlus />
+            </Button>
+          </div>
+        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Nama Orang Tua</TableHead>
+              <TableHead>Jumlah Anak</TableHead>
+              <TableHead>Alamat</TableHead>
+              <TableHead>Jenis Kelamin (p / l) </TableHead>
+              <TableHead>Tinggi Badan (cm)</TableHead>
+              <TableHead>Edit Data</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {parentTableData && parentTableData.map((parent) => (
               <TableRow>
-                <TableHead>Nama Orang Tua</TableHead>
-                <TableHead>Jumlah Anak</TableHead>
-                <TableHead>Alamat</TableHead>
-                <TableHead>Jenis Kelamin (p / l) </TableHead>
-                <TableHead>Tinggi Badan (cm)</TableHead>
-                <TableHead>Berat Badan (kg)</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Edit Data</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell>Asep</TableCell>
+                <TableCell>{parent.name}</TableCell>
                 <TableCell>2</TableCell>
-                <TableCell>Jl Manja</TableCell>
-                <TableCell>l</TableCell>
-                <TableCell>xx</TableCell>
-                <TableCell>xx</TableCell>
-                <TableCell>Normal</TableCell>
+                <TableCell>{parent.address}</TableCell>
+                <TableCell>{parent.gender === 1 ? 'Laki-laki' : 'Perempuan'}</TableCell>
+                <TableCell>{parent.height}</TableCell>
                 <TableActionButton />
               </TableRow>
-            </TableBody>
-          </Table>
-        </TabsContent>
-      </Tabs>
-    </section>
-  );
+            ))}
+          </TableBody>
+        </Table>
+      </TabsContent>
+    </Tabs>
+  </section>;
 }
 
 function TableActionButton() {
@@ -167,7 +201,7 @@ function TableActionButton() {
       <Dialog>
         <DialogTrigger asChild>
           <button className={`bg-red-500 flex ${true ? "rounded" : "rounded-md"} items-center gap-x-1 text-white px-3 py-2`}>
-            <FaTrashAlt /> <span>Dellete</span>
+            <FaTrashAlt /> <span>Delete</span>
           </button>
         </DialogTrigger>
         <DialogContent className="bg-white">
