c8105842277358ebbcc13547c5928bb3c1d8e8d0
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-07-13 00:26:50 +0700

    feat: create children

--- src/components/dashboard/MainDashboard.jsx
+++ src/components/dashboard/MainDashboard.jsx
@@ -23,6 +23,8 @@ import {Button} from "@/components/ui/button";
 import axios from "axios";
 import FormDashbboard from "./FormDashbboard";
 import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group.jsx";
+import Select from "react-select";
+import DatePicker from "react-datepicker";
 
 export default function MainDashboard() {
   const [parentTableData, setParentTableData] = useState([])
@@ -57,6 +59,7 @@ export default function MainDashboard() {
   useEffect(() => {
     getParentTableData()
     getChildTableData()
+
   }, []);
 
   return <section className="h-screen font-poppins">
@@ -98,9 +101,7 @@ export default function MainDashboard() {
             <Input className="rounded-full border-slate-400" placeholder="Search"/>
           </div>
           <div className="w-full text-end">
-            <Button>
-              <FaPlus/>
-            </Button>
+            <CreateNewChildrenFormDialog />
           </div>
         </div>
         <Table>
@@ -215,7 +216,10 @@ function CreateNewParentFormDialog() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button>
+        <Button
+          className="m-auto flex gap-4 justify-center items-center bg-primaryFigma text-white border border-primaryFigma hover:text-primaryFigma rounded-full shadow-md shadow-secondaryFigma"
+          type="submit">
+          Orang tua
           <FaPlus/>
         </Button>
       </DialogTrigger>
@@ -270,7 +274,160 @@ function CreateNewParentFormDialog() {
             </div>
           </div>
           <DialogFooter className="mt-5">
-            <Button type="submit" className="bg-primaryFigma w-full text-white border border-primaryFigma hover:text-primaryFigma rounded-full shadow-md shadow-secondaryFigma" type="submit">Save changes</Button>
+            <Button type="submit"
+                    className="bg-primaryFigma w-full text-white border border-primaryFigma hover:text-primaryFigma rounded-full shadow-md shadow-secondaryFigma"
+                    type="submit">Save changes</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+function CreateNewChildrenFormDialog() {
+  const payload = {
+    name: '',
+    age: 0,
+    weight: 0,
+    height: 0,
+    birth_date: new Date(),
+    gender: null,
+    parent_id: null,
+  }
+
+  const [formPayload, setFormPaylaod] = useState(payload);
+  const [parentOptions, setParentOptions] = useState([]);
+
+  const getParentOptions = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/parent-options')
+
+      if (response.data) {
+        const options = response.data.data.map((parent) => ({
+          value: parent.value,
+          label: parent.label
+        }))
+
+        setParentOptions(options)
+      }
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  const handleChangeParentOption = (e) => {
+    const temp = {...formPayload}
+    temp.parent_id = e.value
+    setFormPaylaod(temp);
+  }
+
+  const handleFormChange = (e) => {
+    e.preventDefault()
+    const temp = {...formPayload}
+
+    temp[e.target.id] = e.target.value
+    setFormPaylaod((prevState) => ({...prevState, ...temp}))
+  }
+
+  const handleSubmitChildren = async (e) => {
+    e.preventDefault()
+
+    try {
+      const payload = {
+        ...formPayload,
+        age: parseInt(formPayload.age),
+        height: parseInt(formPayload.height),
+        weight: parseInt(formPayload.weight),
+        gender: parseInt(formPayload.gender),
+      }
+
+      const response = await axios.post('http://localhost:8080/api/children/create', payload)
+
+      if (response.data) {
+        console.log(response.data)
+      }
+      console.log(formPayload)
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
+  useEffect(() => {
+    getParentOptions()
+  }, []);
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          className="m-auto flex gap-4 justify-center items-center bg-primaryFigma text-white border border-primaryFigma hover:text-primaryFigma rounded-full shadow-md shadow-secondaryFigma"
+          type="submit">
+          Anak-anak
+          <FaPlus/>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="bg-white font-poppins">
+        <DialogHeader>
+          <DialogTitle>Edit Data Anak</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmitChildren} onChange={handleFormChange}>
+          <div className="flex flex-col gap-y-5">
+            <div className="flex items-center gap-4">
+              <Label className="w-2/6" htmlFor="name">Orang tua</Label>
+              <Select options={parentOptions} onChange={handleChangeParentOption} />
+            </div>
+            <div className="flex items-center gap-4">
+              <Label className="w-2/6" htmlFor="name">Nama Lengkap</Label>
+              <Input placeholder="Masukan Nama Lengkap" id="name"
+                     className={`w-full border-slate-400 rounded-full placeholder:text-slate-400`}/>
+            </div>
+            <div className="flex items-center gap-4">
+              <Label className="w-2/6" htmlFor="age">Umur</Label>
+              <Input placeholder="Umur" id="umur"
+                     className={`w-full border-slate-400 rounded-full placeholder:text-slate-400`}/>
+            </div>
+            <div className="flex items-center gap-4">
+              <Label className="w-2/6" htmlFor="age">Berat badan (Kg)</Label>
+              <Input placeholder="20kg" id="weight"
+                     className={`w-full border-slate-400 rounded-full placeholder:text-slate-400`}/>
+            </div>
+            <div className="flex items-center gap-4">
+              <Label className="w-2/6" htmlFor="height">Tinggi badan (cm)</Label>
+              <Input placeholder="160cm" id="height"
+                     className={`w-full border-slate-400 rounded-full placeholder:text-slate-400`}/>
+            </div>
+            <div className="flex items-center gap-4">
+              <Label className="w-2/6" htmlFor="birth_date">Tanggal Lahir</Label>
+              <DatePicker
+                selected={formPayload.birth_date}
+                onChange={handleFormChange}
+                dateFormat="yyyy-MM-dd"
+              />
+            </div>
+            <div className="flex items-center gap-x-10 mx-5">
+              <Label htmlFor="name">Jenis Kelamin</Label>
+              <RadioGroup className="flex gap-x-5 mt-3 justify-start" defaultValue="Ya">
+                <div className={`flex items-center rounded-full gap-x-3 bg-secondaryFigma justify-center px-3 w-full`}>
+                  <RadioGroupItem value="1" id="r1"/>
+                  <Label htmlFor="r1">
+                    Laki-Laki
+                  </Label>
+                </div>
+                <div
+                  className={`flex items-center rounded-full gap-x-3 bg-secondaryFigma justify-center px-3 py-1 w-full`}>
+                  <RadioGroupItem value="2" id="r2"/>
+                  <Label htmlFor="r2">
+                    Perempuan
+                  </Label>
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+          <DialogFooter className="mt-5">
+            <Button type="submit"
+                    className="bg-primaryFigma w-full text-white border border-primaryFigma hover:text-primaryFigma rounded-full shadow-md shadow-secondaryFigma"
+                    type="submit">Save changes</Button>
           </DialogFooter>
         </form>
       </DialogContent>
